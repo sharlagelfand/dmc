97816ae2daf1e93d45b92d576a8099de23543aa2
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-02-09 10:38:59 -0500

    Add text for knit output

--- tests/testthat/test-dmc_df.R
+++ tests/testthat/test-dmc_df.R
@@ -16,7 +16,21 @@ test_that("print.dmc_df only returns the df if there is no viz", {
 test_that("print.dmc_df returns df and viz if there is one (and knitr is not in progress).", {
   col <- "#000000"
   x <- dmc(col)
+  expect_s3_class(attr(x, "visualization"), "magick-image")
   expect_equal(capture_output(print(x)), "# A tibble: 1 x 6\n  dmc   name  hex       red green  blue\n  <chr> <chr> <chr>   <dbl> <dbl> <dbl>\n1 310   Black #000000     0     0     0")
 })
 
-# TODO: add test for visualization properly rendering in Rmd
+test_that("if knitr is in progress, print.dmc_df returns the df AND the visualization via knitr::include_graphics which converts to ![]() in the md", {
+  skip_on_cran()
+  rmd_lines <- c("```{r}\n", "library(dmc)\n", "dmc('#000000')\n", "```")
+  tmp_file <- tempfile()
+  tmp_file_rmd <- paste0(tmp_file, ".rmd")
+  tmp_file_md <- paste0(tmp_file, ".md")
+  cat(rmd_lines, file = tmp_file_rmd)
+  knitr::knit(tmp_file_rmd, output = tmp_file_md)
+  output <- readLines(tmp_file_md)
+  expect_equal(output, c("", "```r", " library(dmc)", " dmc('#000000')",
+                         "```", "", "```", "## # A tibble: 1 x 6", "##   dmc   name  hex       red green  blue",  "##   <chr> <chr> <chr>   <dbl> <dbl> <dbl>", "## 1 310   Black #000000     0     0     0", "```", "","![plot of chunk unnamed-chunk-1](figure/unnamed-chunk-1-1.png)"))
+  file.remove(tmp_file_rmd)
+  file.remove(tmp_file_md)
+})
